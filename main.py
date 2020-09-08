7891a22c5c69660900e143a97155d65ceff20acc
Ollie Shadbolt <ollieshadbolt@gmail.com>
2020-09-09 00:27:10 +0100

    First implemetation

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+#! /usr/bin/env python3
+"""
+A Python 3 script for my r34 Discord bot.
+"""
 import urllib.request
 import xmltodict
 import random
 import discord
 
+__version__ = '1.0'
 
 client = discord.Client()
 
 
 def get_data(url):
-    with urllib.request.urlopen(url) as fp:
-        mybytes = fp.read()
+    """
+    Returns data from a given url.
+    """
+    with urllib.request.urlopen(url) as file:
+        xml = file.read()
 
-    return xmltodict.parse(mybytes.decode("utf8"))
+    return xmltodict.parse(xml.decode("utf8"))
 
 
 def get_post(tags):
+    """
+    Returns a random post from rule34.xxx using parameter tags.
+    """
     url = "https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=" + tags
     data = get_data(url)
     posts = 'posts'
@@ -22,40 +33,45 @@ def get_post(tags):
 
     if count == 0:
         return None
-    
+
     items = 100
 
     if count <= items:
-        randint = random.randint(0, count)
+        index = random.randint(0, count)
 
     else:
         limit = 200000
-        
+
         if count > limit:
             count = limit
 
-        randint = random.randint(0, count)
-        data = get_data(url + "&pid=" + str((randint // items) - 1))
-        randint %= 100
+        index = random.randint(0, count)
+        data = get_data(url + "&pid=" + str((index // items) - 1))
+        index %= 100
 
-    return data[posts]['post'][randint]['@file_url']
+    return data[posts]['post'][index]['@file_url']
 
 
 @client.event
 async def on_message(message):
     content = message.content
-    prefix = "r34"
-    
-    if not content.startswith(prefix):
+    prefix = "+r34 "
+
+    if not (content.startswith(prefix) and message.channel.is_nsfw()):
         return
 
-    await message.channel.send(content=get_post(content[len(prefix):]))
+    content = get_post(content[len(prefix):])
+
+    if content is None:
+        content = "No results."
+
+    await message.channel.send(content=content)
 
 
 def main():
-    with open('token.txt') as fp:
-        token = fp.read()
-    
+    with open('token.txt') as file:
+        token = file.read()
+
     client.run(token)
 
 
